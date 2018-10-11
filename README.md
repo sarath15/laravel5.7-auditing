6c5c329b07df906b7900a8d9d3e65a0bba6fb13a
SarathkumarV_Contus <sarathkumar.v@contus.in>
2018-10-11 20:56:01 +0530

    Added the package for user management with audit functionality

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # laravel5.7-auditing
 Laravel 5.7 application with user Auditing.
+
+Setup
+
+run the following commands
+
+# Composer install
+
--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Auditable
 {
-    use Notifiable;
+    use Notifiable, \OwenIt\Auditing\Auditable;
 
     /**
      * The attributes that are mass assignable.
